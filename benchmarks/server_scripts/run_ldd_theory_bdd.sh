50bcf5f8967e78f7290080e15f403d05481f5def
MaxMicheluttiUnitn <massimo.michelutti@studenti.unitn.it>
2024-03-05 15:06:18 +0100

    .

--- benchmarks/server_scripts/run_ldd_theory_bdd.sh
+++ benchmarks/server_scripts/run_ldd_theory_bdd.sh
@@ -13,34 +13,35 @@ do
 	smtfilename="${file#"$lddbenchfolder"}"
 	jsonfilename="${smtfilename/.smt/.json}"
 	tmpfile="${file/LDD_bench_no_exists/tmp}"
-	#echo "$smtfilename"
-	#echo "$jsonfilename"
-	if [ -f "$outputfolder""$jsonfilename" ]; then
-		echo "Skipping task on $smtfilename"
-	else
-		echo "Performing task on $smtfilename"
-		if [ -f "$tmpfile" ]; then
-timeout 3600s python main.py -i "$lddbenchfolder""$smtfilename" --load_lemmas "$tmpfile" --bdd --count_models --count_nodes -d "$outputfolder""$jsonfilename"
-			if [ $? -eq 0 ]; then
-				echo "Task completed for $smtfilename"
-			else
-				echo "Timeout for $smtfilename"
-				echo "{\"timeout\":\"DD\"}" > "$outputfolder""$jsonfilename"
-			fi
-		else
-timeout 3600s python main.py -i "$lddbenchfolder""$smtfilename" --save_lemmas "$tmpfile"
-			if [ $? -eq 0 ]; then
-timeout 3600s python main.py -i "$lddbenchfolder""$smtfilename" --load_lemmas "$tmpfile" --bdd --count_models --count_nodes -d "$outputfolder""$jsonfilename"
-				if [ $? -eq 0 ]; then
-					echo "Task completed for $smtfilename"
-				else
-					echo "Timeout for $smtfilename"
-					echo "{\"timeout\":\"DD\"}" > "$outputfolder""$jsonfilename"
-				fi
-			else
-				echo "Timeout for $smtfilename"
-				echo "{\"timeout\":\"ALL SMT\"}" > "$outputfolder""$jsonfilename"
-			fi
-		fi
-	fi
+	echo "$smtfilename"
+	echo "$jsonfilename"
+	echo "$tmpfile"
+# 	if [ -f "$outputfolder""$jsonfilename" ]; then
+# 		echo "Skipping task on $smtfilename"
+# 	else
+# 		echo "Performing task on $smtfilename"
+# 		if [ -f "$tmpfile" ]; then
+# timeout 3600s python main.py -i "$lddbenchfolder""$smtfilename" --load_lemmas "$tmpfile" --bdd --count_models --count_nodes -d "$outputfolder""$jsonfilename"
+# 			if [ $? -eq 0 ]; then
+# 				echo "Task completed for $smtfilename"
+# 			else
+# 				echo "Timeout for $smtfilename"
+# 				echo "{\"timeout\":\"DD\"}" > "$outputfolder""$jsonfilename"
+# 			fi
+# 		else
+# timeout 3600s python main.py -i "$lddbenchfolder""$smtfilename" --save_lemmas "$tmpfile"
+# 			if [ $? -eq 0 ]; then
+# timeout 3600s python main.py -i "$lddbenchfolder""$smtfilename" --load_lemmas "$tmpfile" --bdd --count_models --count_nodes -d "$outputfolder""$jsonfilename"
+# 				if [ $? -eq 0 ]; then
+# 					echo "Task completed for $smtfilename"
+# 				else
+# 					echo "Timeout for $smtfilename"
+# 					echo "{\"timeout\":\"DD\"}" > "$outputfolder""$jsonfilename"
+# 				fi
+# 			else
+# 				echo "Timeout for $smtfilename"
+# 				echo "{\"timeout\":\"ALL SMT\"}" > "$outputfolder""$jsonfilename"
+# 			fi
+# 		fi
+# 	fi
 done
