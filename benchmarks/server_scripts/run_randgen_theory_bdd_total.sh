81a8361c48e79166f0331789e4534f3a115b5fb1
MaxMicheluttiUnitn <massimo.michelutti@studenti.unitn.it>
2024-03-14 15:36:38 +0100

    added server script for randgen using total solver

---
+++ benchmarks/server_scripts/run_randgen_theory_bdd_total.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+mkdir ./benchmarks/randgen/output_total
+mkdir ./benchmarks/randgen/tmp
+
+
+for gen in ./benchmarks/randgen/data/*
+do
+	outputgen="${gen/data/output_total}"
+	tmpgen="${gen/data/tmp}"
+	mkdir $outputgen
+	mkdir $tmpgen
+	for probs in $gen/*
+	do
+		outputprobs="${probs/data/output_total}"
+		tmpprobs="${probs/data/tmp}"
+		mkdir $outputprobs
+		mkdir $tmpprobs
+		for item in $probs/*
+		do
+			smtfilename="${item#"$probs"/}"
+			jsonfilename="${smtfilename/.smt2/.json}"
+			tmpfile="${item/data/tmp}"
+			#echo $smtfilename
+			#echo $jsonfilename
+			#echo $tmpfile
+			if [ -f "$outputprobs"/"$jsonfilename" ]; then
+				echo "Skipping task on $smtfilename"
+			else
+				echo "Performing task on $smtfilename"
+				if [ -f "$tmpfile" ]; then
+					timeout 3600s python main.py -i "$item" --load_lemmas "$tmpfile" --load_details "$outputprobs"/"$jsonfilename" --tbdd --count_nodes --count_models -d "$outputprobs"/"$jsonfilename"
+					if [ $? -eq 0 ]; then
+						echo "Task completed on $smtfilename"
+					else
+						echo "Timeout on $smtfilename"
+						echo "{\"timeout\":\"DD\"}" > "$outputprobs"/"$jsonfilename"
+					fi
+				else
+					timeout 3600s python main.py -i "$item" --save_lemmas "$tmpfile" --solver total -d "$outputprobs"/"$jsonfilename"
+					if [ $? -eq 0 ]; then
+						timeout 3600s python main.py -i "$item" --load_lemmas "$tmpfile" --load_details "$outputprobs"/"$jsonfilename" --tbdd --count_nodes --count_models -d "$outputprobs"/"$jsonfilename"
+						if [ $? -eq 0 ]; then
+							echo "Task completed on $smtfilename"
+						else
+							echo "Timeout on $smtfilename"
+							echo "{\"timeout\":\"DD\"}" > "$outputprobs"/"$jsonfilename"
+						fi
+					else
+						echo "Timeout on $smtfilename"
+						echo "{\"timeout\":\"ALL SMT\"}" > "$outputprobs"/"$jsonfilename"
+					fi
+				fi
+			fi
+		done
+	done
+done
